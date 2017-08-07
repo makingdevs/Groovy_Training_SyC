037a0ee48be3857c8579605e422e06fef4490686
neodevelop <juan@makingdevs.com>
2017-08-07 15:35:47 -0500

    Paso 1: Quitando `public` y `;`

--- HolaMundo.groovy
+++ HolaMundo.groovy
@@ -1,18 +1,18 @@
-public class HolaMundo {
-  private String nombre;
+class HolaMundo {
+  private String nombre
 
-  public void setNombre(String nombre){
-    this.nombre = nombre;
+  void setNombre(String nombre){
+    this.nombre = nombre
   }
-  public String getNombre(){
-    return this.nombre;
+  String getNombre(){
+    return this.nombre
   }
-  public String saluda(){
-    return "Hola " + this.nombre + " !!!";
+  String saluda(){
+    return "Hola " + this.nombre + " !!!"
   }
-  public static void main(String[] args){
-    HolaMundo objeto = new HolaMundo();
-    objeto.setNombre("Juan0");
-    System.out.println(objeto.saluda());
+  static void main(String[] args){
+    HolaMundo objeto = new HolaMundo()
+    objeto.setNombre("Juan1")
+    System.out.println(objeto.saluda())
   }
 }
