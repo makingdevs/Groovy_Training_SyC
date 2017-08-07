0224aacfcced05386e85f221219568c7a63ba276
neodevelop <juan@makingdevs.com>
2017-08-07 15:26:20 -0500

    Agregando primeros ejercicios, HolaMundo en Java

---
+++ HolaMundo.java
@@ -0,0 +1,18 @@
+public class HolaMundo {
+  private String nombre;
+
+  public void setNombre(String nombre){
+    this.nombre = nombre;
+  }
+  public String getNombre(){
+    return this.nombre;
+  }
+  public String saluda(){
+    return "Hola " + this.nombre + " !!!";
+  }
+  public static void main(String[] args){
+    HolaMundo objeto = new HolaMundo();
+    objeto.setNombre("Juan");
+    System.out.println(objeto.saluda());
+  }
+}
