77d00363c8d13d08f4aead94ca417b20f33236e5
neodevelop <juan@makingdevs.com>
2017-08-07 15:50:04 -0500

    Paso 6 : POGO's

--- HolaMundo.groovy
+++ HolaMundo.groovy
@@ -5,8 +5,13 @@ class HolaMundo {
     "Hola $nombre !!!"
   }
   static main(args){
-    def objeto = new HolaMundo()
-    objeto.setNombre("Juan5")
+    def objeto = new HolaMundo(nombre:"Juan6")
+    println(objeto.saluda())
+    objeto.setNombre("SyC")
+    println(objeto.saluda())
+    objeto.nombre = "Groovy"
+    println(objeto.saluda())
+    objeto['nombre'] = "Training"
     println(objeto.saluda())
   }
 }
