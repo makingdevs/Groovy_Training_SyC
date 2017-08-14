64b49e8ef0b46dbb075de04df5287aed4bf2ba38
neodevelop <juan@makingdevs.com>
2017-08-14 16:20:08 -0500

    Parseando con XmlParser

---
+++ modulo2/parser.groovy
@@ -0,0 +1,8 @@
+entrenamientoParser = new XmlParser().parse('makingdevs.xml')
+println "Entrenamiento en ${entrenamientoParser.'@organization'}"
+entrenamientoParser.roadmap.category.each { category ->
+  println "- ${category.'@name'}"
+  category.course.each { course ->
+    println "\t-${course.text().trim()} (${course.'@duration'} hrs. / ${course.'@sessions'} sessions)"
+  }
+}
