a8a267ea334f0ecd81bcf059fffe42214957091e
neodevelop <juan@makingdevs.com>
2017-08-14 14:06:25 -0500

    Usando `memoize`

--- benchmark.groovy
+++ benchmark.groovy
@@ -9,6 +9,21 @@ def fibonacci(n){
   n < 2 ? 1 : fibonacci(n-1) + fibonacci(n-2)
 }
 
+serie = [0: 1, 1: 1]
+def fibonacciAlmacenado(n) {
+  if(!serie.containsKey(n)){
+    serie[n] = fibonacciAlmacenado(n-1) + fibonacciAlmacenado(n-2)
+  }
+  serie[n]
+}
+
+def fibonacciMemoized
+fibonacciMemoized = { n ->
+  n < 2 ? 1 : fibonacciMemoized(n-1) + fibonacciMemoized(n-2)
+}.memoize()
+
 tiempo {
-  println fibonacci(50)
+  //println fibonacci(45)
+  //println fibonacciAlmacenado(40)
+  println fibonacciMemoized(40)
 }
