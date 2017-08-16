5604adbfd2e2d46b2d0ab78aa99f492fc5ae53ae
neodevelop <juan@makingdevs.com>
2017-08-16 16:52:22 -0500

    Agrego un evento al click del botón

--- modulo2/swing.groovy
+++ modulo2/swing.groovy
@@ -1,12 +1,17 @@
 builder = new groovy.swing.SwingBuilder()
 
+contador = 0
+
 frame = builder.frame(
   title:"MakingDevs",
   size:[300,200],
   defaultCloseOperation: javax.swing.WindowConstants.EXIT_ON_CLOSE ) {
     flowLayout()
-    label text:"Hello"
-    button text: "Puchurrame!"
+    lbl = label text:"Hello"
+    button text: "Puchurrame!", actionPerformed: {
+      contador++
+      lbl.text = "Pusheado ${contador} veces"
+    }
   }
 
 frame.show()
