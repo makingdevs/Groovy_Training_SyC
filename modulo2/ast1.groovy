2ab5c56e4f3c2c4ce8840511e94b90e01443714d
neodevelop <juan@makingdevs.com>
2017-08-21 13:48:30 -0500

    Varias cosas:
    - Usando anotaciones AST
    - Haciendo la demo de EqualsAndhashCode
    - Demo de TupleConstructor
    - Demo de canonical

--- modulo2/ast1.groovy
+++ modulo2/ast1.groovy
@@ -1,9 +1,21 @@
-@groovy.transform.ToString(includeNames=true)
+//@groovy.transform.ToString(includeNames=true)
+//@groovy.transform.EqualsAndHashCode
+//@groovy.transform.TupleConstructor
+@groovy.transform.Canonical
 class Curso {
   String nombre
   Integer duracion
   BigDecimal costo
-
 }
 
-println new Curso(nombre:"Groovy", duracion:40,costo:3000)
+c1 = new Curso(nombre:"Groovy", duracion:40,costo:3000)
+c2 = new Curso(nombre:"Groovy", duracion:40,costo:3000)
+println c1
+println c2
+println c1 == c2
+c3 = new Curso("VertX")
+c4 = new Curso("VertX", 30)
+c5 = new Curso("VertX", 30, 23.23)
+println c3
+println c4
+println c5
