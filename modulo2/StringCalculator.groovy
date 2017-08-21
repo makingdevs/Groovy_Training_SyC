9fb7089864dc9b98ab8a3109efe697acfc01aef4
neodevelop <juan@makingdevs.com>
2017-08-21 17:53:07 -0500

    Refactor: Making more descriptive

--- modulo2/StringCalculator.groovy
+++ modulo2/StringCalculator.groovy
@@ -1,10 +1,26 @@
 class StringCalculator {
 
   int add(String valor){
-    if(valor=="") return 0
+    if(!valor) return 0
 
-    def expresion = valor?.split(",|\\n")
-    expresion == 0 ? valor.toInteger() : expresion*.toInteger().sum()
+    def expresion = splitTheString(valor)
+    someIsZero(expresion) ? transformASimpleChar(valor)  : sumTheNumbers(expresion)
+  }
+
+  def splitTheString(theString){
+    theString?.split(",|\\n")
+  }
+
+  def sumTheNumbers(numbers){
+    numbers*.toInteger().sum()
+  }
+
+  def transformASimpleChar(simpleChar){
+    simpleChar.toInteger()
+  }
+
+  def someIsZero(some){
+    some == 0
   }
 
 }
