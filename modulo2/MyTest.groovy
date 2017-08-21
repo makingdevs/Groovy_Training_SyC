bd104041a32137c831dfd703022f6fe2d50403d6
neodevelop <juan@makingdevs.com>
2017-08-21 17:09:03 -0500

    Agregando prueba para separador salto de línea

--- modulo2/MyTest.groovy
+++ modulo2/MyTest.groovy
@@ -15,4 +15,20 @@ class MyTest extends GroovyTestCase {
     assert stringCalculator.add("5,5") == 10
     assert stringCalculator.add("5,10") == 15
   }
+
+  void testAnyNumbers(){
+    def stringCalculator = new StringCalculator()
+    assert stringCalculator.add("1,2,3,4,5") == 15
+    assert stringCalculator.add("10,20,30,40,50") == 150
+    assert stringCalculator.add("1,1,1,1,1,1,1,1,1") == 9
+
+  }
+
+  void testAnyNumbersWithNewLines(){
+    def stringCalculator = new StringCalculator()
+    assert stringCalculator.add("1,2\n3\n4,5") == 15
+    assert stringCalculator.add("10\n20,30\n40,50") == 150
+    assert stringCalculator.add("1\n1,1\n1,1\n1,1,1,1") == 9
+
+  }
 }
