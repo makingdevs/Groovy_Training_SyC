8e449136a11fa2673b139dc47caf9d910fb729a8
neodevelop <juan@makingdevs.com>
2017-08-21 17:08:41 -0500

    Respuesta para cualquier cantidad de números

--- modulo2/StringCalculator.groovy
+++ modulo2/StringCalculator.groovy
@@ -1,7 +1,14 @@
 class StringCalculator {
 
   int add(String valor){
-    valor == "" ? 0 : valor.toInteger()
+    if(valor==""){
+      0
+    }
+    else if(valor?.split(",") == 0 ){
+      valor.toInteger()
+    }else{
+      valor.split(",").collect{ it.toInteger() }.sum()
+    }
   }
 
 }
