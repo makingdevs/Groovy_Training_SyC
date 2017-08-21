57ee82cf70e4ddaea3455f21571ca9893e0c9ed9
neodevelop <juan@makingdevs.com>
2017-08-21 17:34:54 -0500

    Refactor: extrayendo variable

--- modulo2/StringCalculator.groovy
+++ modulo2/StringCalculator.groovy
@@ -2,12 +2,13 @@ class StringCalculator {
 
   int add(String valor){
     if(valor==""){
-      0
+      return 0
     }
-    else if(valor?.split(",|\\n") == 0 ){
+    def expresion = valor?.split(",|\\n")
+    if(expresion == 0 ){
       valor.toInteger()
     }else{
-      valor.split(",|\\n").collect{
+      expresion.collect{
         it.toInteger()
       }.sum()
     }
