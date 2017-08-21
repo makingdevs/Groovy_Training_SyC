08dbbd7fba6279f548a82859bf3f3025df46f306
neodevelop <juan@makingdevs.com>
2017-08-21 16:29:34 -0500

    Nueva prueba, 2 números

--- modulo2/MyTest.groovy
+++ modulo2/MyTest.groovy
@@ -8,4 +8,11 @@ class MyTest extends GroovyTestCase {
     def stringCalculator = new StringCalculator()
     assert stringCalculator.add("3") == 3
   }
+
+  void testTwoNumber(){
+    def stringCalculator = new StringCalculator()
+    assert stringCalculator.add("3,5") == 8
+    assert stringCalculator.add("5,5") == 10
+    assert stringCalculator.add("5,10") == 15
+  }
 }
