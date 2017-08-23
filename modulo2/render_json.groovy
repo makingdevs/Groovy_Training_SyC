9ff968f786142c839f77db683b3b61247111881d
neodevelop <juan@makingdevs.com>
2017-08-23 14:53:51 -0500

    Rendering table

---
+++ modulo2/render_json.groovy
@@ -0,0 +1,29 @@
+@GrabConfig(systemClassLoader=true)
+@Grab(group='mysql', module='mysql-connector-java', version='5.1.6')
+
+import groovy.sql.Sql
+import groovy.json.*
+
+db = [
+    url:'jdbc:mysql://localhost/groovy_db',
+    //url:'jdbc:oracle:thin:@myhost:1521:orcl',
+    user:'makingdevs',
+    password:'makingdevs',
+    driver:'com.mysql.jdbc.Driver'
+    //driver:'oracle.jdbc.pool.OracleDataSource'
+]
+
+sql = Sql.newInstance(
+    db.url,
+    db.user,
+    db.password,
+    db.driver)
+
+model = [:]
+model.rows = []
+sql.eachRow("select * from issue limit 0,20") {
+  System.out.println it
+  model.rows << [id: it.id, description:it.description]
+}
+
+json(model)
