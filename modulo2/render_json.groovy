dccde275548a97da15a3cd64aedcc48a101f4203
neodevelop <juan@makingdevs.com>
2017-08-23 15:08:59 -0500

    Agregando la lectura de un JSON

--- modulo2/render_json.groovy
+++ modulo2/render_json.groovy
@@ -4,25 +4,31 @@
 import groovy.sql.Sql
 import groovy.json.*
 
+//request.getProperties().each {
+//  System.out.println it
+//}
+System.out.println request.reader.text
+System.out.println request.method
+
 db = [
-    url:'jdbc:mysql://localhost/groovy_db',
-    //url:'jdbc:oracle:thin:@myhost:1521:orcl',
-    user:'makingdevs',
-    password:'makingdevs',
-    driver:'com.mysql.jdbc.Driver'
-    //driver:'oracle.jdbc.pool.OracleDataSource'
+url:'jdbc:mysql://localhost/groovy_db',
+//url:'jdbc:oracle:thin:@myhost:1521:orcl',
+user:'makingdevs',
+password:'makingdevs',
+driver:'com.mysql.jdbc.Driver'
+//driver:'oracle.jdbc.pool.OracleDataSource'
 ]
 
 sql = Sql.newInstance(
-    db.url,
-    db.user,
-    db.password,
-    db.driver)
+  db.url,
+  db.user,
+  db.password,
+  db.driver)
 
-model = [:]
-model.rows = []
-sql.eachRow("select * from issue limit 0,20") {
-  model.rows << [id: it.id, description:it.description]
-}
-response.contentType = 'application/json'
+  model = [:]
+  model.rows = []
+  sql.eachRow("select * from issue limit 0,20") {
+    model.rows << [id: it.id, description:it.description]
+  }
+  response.contentType = 'application/json'
 json(model)
