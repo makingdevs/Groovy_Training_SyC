50ed6ad144957435bd8dbf9353c7d768c8c214b0
neodevelop <juan@makingdevs.com>
2017-08-23 14:16:03 -0500

    Agregando un groovlet de exploración

---
+++ modulo2/index.groovy
@@ -0,0 +1,15 @@
+html.html{
+  head {
+    title 'Demo de Groovlets'
+  }
+  body {
+    h1 'Bienvenido a los Groovlets de MakingDevs'
+    h4 "$headers"
+    h4 "$params"
+    h4 "$session"
+    h4 "$context"
+    h4 "$application"
+    h4 "$request"
+    h4 "$response"
+  }
+}
