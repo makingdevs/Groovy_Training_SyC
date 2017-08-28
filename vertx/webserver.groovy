fd0c437692789a3b91e4661a133f4860a3119892
neodevelop <juan@makingdevs.com>
2017-08-28 14:39:48 -0500

    Creando un webserver

--- vertx/webserver.groovy
+++ vertx/webserver.groovy
@@ -1,1 +1,11 @@
-println vertx.properties
+server = vertx.createHttpServer()
+
+server.requestHandler { request ->
+
+  def response = request.response()
+  response.putHeader("content-type", "text/plain")
+  response.end("Hello world ${new Date()}")
+
+}
+
+server.listen(8181)
