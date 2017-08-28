ba5e2014316e5a97b865338ef0577a3cd29ccdda
neodevelop <juan@makingdevs.com>
2017-08-28 15:44:57 -0500

    Agregando un NetServer de ejemplo por pregunta

--- vertx/webserver.groovy
+++ vertx/webserver.groovy
@@ -5,6 +5,19 @@ import io.vertx.ext.web.handler.StaticHandler
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
 
+//def server2 = vertx.createNetServer()
+//server2.connectHandler({ socket ->
+//  println "Cliente conectado"
+//  socket.write "${new Date()}"
+//})
+//server2.listen(1234, "localhost", { res ->
+//  if (res.succeeded()) {
+//    println("Server is now listening!")
+//  } else {
+//    println("Failed to bind!")
+//  }
+//})
+
 router.route("/static/*").handler(StaticHandler.create().setCachingEnabled(false))
 
 server.requestHandler(router.&accept).listen(8181)
