cf044803baca625aab2cb77d6386c8cf0ca4a698
neodevelop <juan@makingdevs.com>
2017-08-28 14:57:37 -0500

    Agregando imports faltantes

--- vertx/webserver.groovy
+++ vertx/webserver.groovy
@@ -1,8 +1,11 @@
+import io.vertx.ext.web.Router
+import io.vertx.core.Vertx
+
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
 
 router.route().handler { routingContext ->
-  def response = rountingContext.response()
+  def response = routingContext.response()
   response.putHeader("content-type", "text/plain")
   response.end("Hello world ${new Date()}")
 }
