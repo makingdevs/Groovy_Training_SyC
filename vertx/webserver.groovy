d25485ecb83ad1dbad53e6f44a613fa0aafcbedd
neodevelop <juan@makingdevs.com>
2017-08-28 17:32:26 -0500

    Creando el bridge para vertx

--- vertx/webserver.groovy
+++ vertx/webserver.groovy
@@ -1,23 +1,31 @@
 import io.vertx.ext.web.Router
 import io.vertx.core.Vertx
 import io.vertx.ext.web.handler.StaticHandler
+import io.vertx.ext.web.handler.sockjs.SockJSHandler
+import io.vertx.ext.web.handler.sockjs.BridgeOptions
+import io.vertx.ext.web.handler.sockjs.PermittedOptions
 
+// Ya cree el Webserver!!!
 def server = vertx.createHttpServer()
+// Aqui creo mi router para direccionar toooodoooo el tráfico, de cualquier tipo
+// es decir, http o ws
 def router = Router.router(vertx)
 
-//def server2 = vertx.createNetServer()
-//server2.connectHandler({ socket ->
-//  println "Cliente conectado"
-//  socket.write "${new Date()}"
-//})
-//server2.listen(1234, "localhost", { res ->
-//  if (res.succeeded()) {
-//    println("Server is now listening!")
-//  } else {
-//    println("Failed to bind!")
-//  }
-//})
+// Creo las BridgeOption para mi puente
+BridgeOptions options = new BridgeOptions()
+// Defino que entra y que sale
+//outuboundAddresses = ["clock-client", "clock-status", "system-status"]
+options.addOutboundPermitted(new PermittedOptions().setAddress("clock-client"))
+// Definir mi handler de SockJS
+sockjsHandler = SockJSHandler.create(vertx).bridge(options)
+// Creo la ruta para este bridge
+router.route("/eventbus/*").handler(sockjsHandler)
 
+// Ya cree el manejo de css, js y html
 router.route("/static/*").handler(StaticHandler.create().setCachingEnabled(false))
 
+// Inicio el server
 server.requestHandler(router.&accept).listen(8181)
+
+// Despliego otro verticle
+vertx.deployVerticle("clock.groovy")
--- vertx/webserver.rb
+++ vertx/webserver.rb
@@ -4,6 +4,6 @@ require 'vertx-web/static_handler'
 server = $vertx.create_http_server()
 router = VertxWeb::Router.router($vertx)
 
-router.route("/static/*").handler(&VertxWeb::StaticHandler.create().method(:handle))
+router.route("/static/*").handler(&VertxWeb::StaticHandler.create().set_caching_enabled(false).method(:handle))
 
 server.request_handler(&router.method(:accept)).listen(8181)
