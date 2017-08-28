e1947e39da42f2e0f7e0b5f237a26d7f288f4d42
neodevelop <juan@makingdevs.com>
2017-08-28 15:14:33 -0500

    Usando get y post en el router

--- vertx/webserver.groovy
+++ vertx/webserver.groovy
@@ -4,11 +4,20 @@ import io.vertx.core.Vertx
 def server = vertx.createHttpServer()
 def router = Router.router(vertx)
 
-router.route().handler { routingContext ->
+router.route("/some").handler { routingContext ->
   def response = routingContext.response()
   response.putHeader("content-type", "text/plain")
   response.end("Hello world ${new Date()}")
 }
-
+router.get("/foo").handler { routingContext ->
+  def response = routingContext.response()
+  response.putHeader("content-type", "text/plain")
+  response.end("FOO ${new Date()}")
+}
+router.post("/bar").handler { routingContext ->
+  def response = routingContext.response()
+  response.putHeader("content-type", "text/plain")
+  response.end("BAR ${new Date()}")
+}
 
 server.requestHandler(router.&accept).listen(8181)
